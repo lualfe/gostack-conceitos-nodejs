5b4f803eb466cc9e30a4a1cab174774abbbd64dd
Lucas Albuquerque <lucas.albuquerque.felipini@gmail.com>
2020-04-08 01:40:47 -0300

    First commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,52 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+  repositories.push(repository)
+  return response.json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { title, url, techs } = request.body
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'repository does not exist' })
+  }
+
+  const likes = repositories[repoIndex].likes
+
+  const repo = { id, title, url, techs, likes }
+  repositories[repoIndex] = repo
+  return response.json(repo)
 });
 
 app.delete("/repositories/:id", (req, res) => {
-  // TODO
+  const { id } = req.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+  if (repoIndex < 0) {
+    return res.status(400).json({ error: 'repository does not exist' })
+  }
+  repositories.splice(repoIndex, 1)
+  return res.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id)
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'repository does not exist' })
+  }
+  repositories[repoIndex].likes++
+  return response.json(repositories[repoIndex])
 });
 
 module.exports = app;
